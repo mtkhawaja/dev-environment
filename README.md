67e43e2644f7cd8d016823dd4f69030392ec8b45
Muneeb Khawaja <muneebtkhawaja@gmail.com>
2024-01-01 03:44:06 -0500

    feat: Added -y to setup script.

--- README.md
+++ README.md
@@ -27,9 +27,9 @@ Alternatively, you can run the commands below manually:
 # Install Ansible
 
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install -y ansible
 
 # Run the playbook
 
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install -y ansible
 ansible-pull -U https://github.com/mtkhawaja/dev-environment.git -i localhost, local.yaml
